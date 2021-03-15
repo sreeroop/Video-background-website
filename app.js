cb426a0f0cb11bcf46941effb60bf1f88dfa7885
sreeroop <sreeroopsk10@gmail.com>
2021-03-16 02:32:24 +0530

    test1

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  trigger: intro,
+  triggerHook: 0
+})
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
+
 const hamburger = document.querySelector(".hamburger");
 
 const navLinks = document.querySelector(".nav-links");
@@ -11,44 +57,3 @@ hamburger.addEventListener("click", () => {
     links.classList.toggle("fade");
   }); */
 });
-
-
-const intro = document.querySelector(".intro");
-const video = document.querySelector("video");
-const text = document.querySelector("h1");
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
-
-const controller = new ScrollMagic.Controller();
-
-let scene = new ScrollMagic.Scene({
-    duration: 6000,
-    trigger: intro,
-    triggerHook: 0
-})
-.setPin(intro)
-.addTo(controller);
-
-const textAnim = TweenMax.fromTo(text, 3,{opacity: 1 },{opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-    duration: 3000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-.setTween(textAnim)
-.addTo(controller);
-
-let accelamount = 1;
-let scrollpos = 0;
-let delay = 0.5;
-scene.on('update', e =>{
-    scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(()=>{
-    delay += (scrollpos - delay) * accelamount;
-    video.currentTime =  scrollpos;
-}, 16.6667);
-
-video.play
