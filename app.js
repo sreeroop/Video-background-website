99594972dd7db0f9e4ed250b7ad77216869395d5
sreeroop <sreeroopsk10@gmail.com>
2021-03-16 18:47:05 +0530

    HEAD text removed

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
-<<<<<<< HEAD
   duration: 5000,
-=======
-  duration: 7000,
->>>>>>> d9159b4b33043e5ea9fba24a98f3b06a15ad8e90
   trigger: intro,
   triggerHook: 0
 })
@@ -46,11 +42,7 @@ setInterval(() => {
   console.log(scrollpos, delay);
 
   video.currentTime = delay;
-<<<<<<< HEAD
 }, 50);
-=======
-}, 20);
->>>>>>> d9159b4b33043e5ea9fba24a98f3b06a15ad8e90
 
 const hamburger = document.querySelector(".hamburger");
 
@@ -61,7 +53,4 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 
-  /* links.forEach((links) => {
-    links.classList.toggle("fade");
-  }); */
 });
